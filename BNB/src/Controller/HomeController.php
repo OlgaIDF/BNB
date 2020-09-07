196529029a5662b51ea1a82e8e69be6c669f474d
Olha <olha.idf@gmail.com>
2020-09-07 15:30:12 +0200

    creation de footer

--- BNB/src/Controller/HomeController.php
+++ BNB/src/Controller/HomeController.php
@@ -13,7 +13,7 @@ class HomeController extends AbstractController
     public function index()
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => ' c\'est cool!',
+            'maVariable' => ' c\'est cool',
         ]);
     }
 }
